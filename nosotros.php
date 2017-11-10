b87c0e89630db2bd1611a30ca05ecb77412e7708
luizink <luizupdate@gmail.com>
2017-11-10 17:11:11 -0500

    NOSOTROS

--- nosotros.php
+++ nosotros.php
@@ -160,7 +160,7 @@
               <ul class="pi-tabs-navigation pi-tabs-navigation-justified pi-tabs-navigation-transparent pi-responsive-sm" id="myTabs1">
 					<li class="pi-active"><a href="#history"><i class="icon-book-open"></i>Misión</a></li>
 					<li><a href="#vision"><i class="icon-eye"></i>Visión</a></li>
-					<li><a href="#phylosophy"><i class="icon-flash"></i>Valores</a></li>
+					<li><a href="#phylosophy"><i class="icon-flash"></i>Filosofía</a></li>
 				</ul>
 				<!-- End tabs -->
 
@@ -170,7 +170,8 @@
 					<!-- Tab content item -->
 					<div class="pi-tab-pane pi-active" id="history">
 						<p>
-							<span class="pi-dropcap">S</span>omos una institución que promueve la investigación, análisis, estudio e implementación de políticas públicas en el Perú y América Latina y desarrolla las capacidades de las personas que desarrollan las políticas públicas o que forman parte de la gestión pública.
+							<span class="pi-dropcap">S</span>omos una institución que promueve la investigación, análisis, estudio e implementación de políticas públicas en el Perú y América Latina y fortalece las capacidades de las personas que desarrollan las políticas públicas, que forman parte de la gestión pública o están interesadas en el desarrollo de los ciudadanos a través de la intervención pública.
+
 						</p>
 					</div>
 					<!-- End tab content item -->
@@ -186,12 +187,13 @@
 					<!-- Tab content item -->
 					<div class="pi-tab-pane" id="phylosophy">
 						<p>
-							<span class="pi-dropcap">C</span>onsideramos fundamental trabajar con valores éticos que guíen su actividad cotidiana, por eso, nos comprometemos a actuar con: Responsabilidad personal y social: reconocer, aceptar y asumir las consecuencias de los actos en la dimensión personal y social son fundamentales para lograr los objetivos de desarrollo institucional que nos proponemos. 
-
-							<ul>
-								<li>Respeto: tener consideración por el otro, aceptando las diferencias, nos permite incorporar políticas públicas inclusivas.</li>
-								<li>Compromiso con las personas: enfocarnos en el desarrollo personal y profesional de las personas es sustancial para mejorar la gestión pública. Son las personas las que actúan en todos los niveles del Estado y del gobierno y por tanto, afianzar sus conocimientos técnicos, profesionales y éticos son la clave para el desarrollo social.</li>
-								<li>Ética: las elecciones personales de todos los involucrados en la actividad pública son determinantes para el desarrollo. Por eso, es necesario fomentar y promover que las elecciones cotidianas y profesionales tengan en cuenta el resultado y las consecuencias a largo plazo y ante la duda, elegir hacer lo correcto</li>
+							<span class="pi-dropcap">Q</span>ueremos combinar la experiencia y el conocimiento académico, para lograr las mejores prácticas en la gestión y políticas públicas. <br> Para ello, en cada una de las líneas de acción:
+						</p><br>
+						<p>
+							<ul class="pi-list-with-icons pi-list-icons-ok el justi">
+								<li><strong>Respeto:</strong> Tener consideración por el otro, aceptando las diferencias, nos permite incorporar políticas públicas inclusivas.</li>
+								<li> <strong>Compromiso con las personas:</strong> Enfocarnos en el desarrollo personal y profesional de las personas es sustancial para mejorar la gestión pública. Son las personas las que actúan en todos los niveles del Estado y del gobierno y por tanto, afianzar sus conocimientos técnicos, profesionales y éticos son la clave para el desarrollo social.</li>
+								<li><strong>Ética:</strong> Las elecciones personales de todos los involucrados en la actividad pública son determinantes para el desarrollo. Por eso, es necesario fomentar y promover que las elecciones cotidianas y profesionales tengan en cuenta el resultado y las consecuencias a largo plazo y ante la duda, elegir hacer lo correcto</li>
 							</ul>
 						</p>
 					</div>
@@ -211,36 +213,107 @@
 					<em>Observatorio Latinoamericano de Gestión y Desarrollo</em>
 				</p>
 
-				<p>
+				<p class="justi">
 					Somos una Asociación Civil que tiene por finalidad promover la investigación, análisis, estudio y promoción de políticas públicas en el Perú y América Latina; difundir el conocimiento en administración y gestión en general; y especialmente fortalecer capacidades para las personas que forman parte de la gestión pública o que desarrollan políticas públicas.
 				</p>
 
 			</div>
+
 		</div>
+
+<!-- ESTRUCTURA -->
+<hr class="pi-divider-gap-60">
+<!-- Values -->
+		<div class="pi-row">
+
+			<!-- Value item -->
+			<div class="pi-col-sm-12">
+				<div class="pi-icon-box">
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
+						<i class="icon-qq"></i>
+					</div>
+					<div class="pi-icon-box-content-2 justi">
+						<h4>
+							Estructura Organizativa:
+						</h4>
+						<p>
+							La administración y representación legal de la asociación está a cargo del Consejo Directivo, integrado por 3 miembros elegidos por la Asamblea General. Uno de ellos, es elegido como Presidente del Consejo Directivo. El mandato del Consejo Directivo es de 3 años y sus miembros pueden ser reelegidos.
+						</p>
+						
+					</div>
+				</div>
+			</div>
+			<!-- End value item -->
+
+			</div>
+<!-- FINESTRUCTURA -->
+
+<!-- ESTRUCTURA -->
+<hr class="pi-divider-gap-60">
+<!-- Values -->
+		<div class="pi-row">
+
+			<!-- Value item -->
+			<div class="pi-col-sm-12">
+				<div class="pi-icon-box">
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
+						<i class="icon-qq"></i>
+					</div>
+					<div class="pi-icon-box-content-2 justi">
+						<h4>
+							Director Académico: Juan José Martínez Ortiz
+						</h4>
+						<p>
+						Abogado, Magíster en Derecho, con estudios de Economía y Regulación de Servicios Públicos a nivel de Maestría. Asesor Legal con más de 18 años de experiencia profesional en la asesoría de entidades de los Sectores Público y Privado. Especialista en Derecho Mercantil, Derecho Administrativo, Gestión Pública, Sistemas Administrativos del Estado, Promoción de la Inversión Privada, Regulación de Servicios Públicos y Análisis Económico del Derecho. Profesor Universitario en cursos de su especialidad. Fluidez en el idioma inglés.
+					</p>
+					
+					<div class="pi-img-w pi-img-round-corners pi-img-shadow pi-img-left pi-margin-top-5" style="width: 200px;">
+						<img src="img_external/gallery/img-31.jpg" alt=""/>
+					</div><br>
+					<p>
+						Ha prestado servicios para las siguientes entidades:<br> <br>
+						<strong> Entidades del Sector Público:</strong> <br>
+						Presidencia del Consejo de Ministros, Ministerio de Economía y Finanzas, Ministerio de Relaciones Exteriores, Ministerio de Justicia y Derechos Humanos, Ministerio de Educación, Ministerio del Ambiente, PROINVERSION, OSINERGMIN, OSITRAN, SERVIR, OEFA, Consejo Nacional de Competividad. <br><br>
+
+						<strong>Entidades del Sector Privado:</strong> <br>Estudio Martínez Zamora Abogados, Energy Partners, Apoyo Consultoría, Consorcio de Investigación Económica Social - CIES, Governa SAC, Grupo de Análisis para el Desarrollo - GRADE, Chemonics International, Checchi & company consulting, GFA Consulting Group, Nathan Associates, Institute of Public Adminstration of Canada, RTI International, IRIS Center. <br>
+
+						<strong>Entidades de la Cooperación Internacional:</strong> <br>HELVETAS Swiss Intecooperation PERU, USAID, GIZ, Fondo de Cooperación Hispano - Peruano. Entidades Académicas: Universidad del Pacífico, Pontificia Universidad Católica del Perú, Universidad Peruana de Ciencias Aplicadas, Universidad San Martín de Porres, Universidad ESAN, Universidad Continental.
+
+					</p>
+					
+					<hr class="pi-divider-gap-10">
+						
+					</div>
+				</div>
+			</div>
+			<!-- End value item -->
+
+			</div>
+<!-- FINESTRUCTURA -->
 
 	
 		<hr class="pi-divider-gap-60">
 
 		<h2 class="h4 pi-weight-700 pi-uppercase pi-letter-spacing pi-has-bg pi-margin-bottom-30">
-			Nuestra Filosofía
+			Nuestros Valores
 		</h2>
+		<p><strong>OLAGED</strong> considera fundamental trabajar con valores éticos que guíen su actividad cotidiana, por eso, nos comprometemos a actuar con:</p> <br>
 
 		<!-- Values -->
 		<div class="pi-row">
 
 			<!-- Value item -->
-			<div class="pi-col-sm-4">
+			<div class="pi-col-sm-12">
 				<div class="pi-icon-box">
-					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #ee7954;">
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
 						<i class="icon-qq"></i>
 					</div>
-					<div class="pi-icon-box-content-2">
+					<div class="pi-icon-box-content-2 justi">
 						<h4>
-							RESPONSABILIDAD
+							Responsabilidad personal y social:
 						</h4>
 						<p>
-							personal y social: reconocer, aceptar y asumir las consecuencias de los actos en la dimensión personal y social son fundamentales para lograr los objetivos de desarrollo institucional que nos proponemos.
-
+							Reconocer, aceptar y asumir las consecuencias de los actos en la dimensión personal y social son fundamentales para lograr los objetivos de desarrollo institucional que nos proponemos.
 						</p>
 						<hr class="pi-divider-gap-10">
 					</div>
@@ -249,37 +322,38 @@
 			<!-- End value item -->
 
 			<!-- Value item -->
-			<div class="pi-col-sm-4">
+			<div class="pi-col-sm-12">
 				<div class="pi-icon-box">
-					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #f06793;">
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
 						<i class="icon-heart"></i>
 					</div>
-					<div class="pi-icon-box-content-2">
+					<div class="pi-icon-box-content-2 justi">
 						<h4>
-							RESPETO
+							Respeto
 						</h4>
 						<p>
-							tener consideración por el otro, aceptando las diferencias, nos permite incorporar políticas públicas inclusivas.
+							Tener consideración por el otro, aceptando las diferencias, nos permite incorporar políticas públicas inclusivas.
+
 						</p>
-						<hr class="pi-divider-gap-10">
+						<hr class="pi-divider-gap-12">
 					</div>
 				</div>
 			</div>
 			<!-- End value item -->
 
 			<!-- Value item -->
-			<div class="pi-col-sm-4">
+			<div class="pi-col-sm-12">
 
 				<div class="pi-icon-box">
-					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #ebba63;">
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
 						<i class="icon-hourglass"></i>
 					</div>
-					<div class="pi-icon-box-content-2">
+					<div class="pi-icon-box-content-2 justi">
 						<h4>
-							COMPROMISO
+							Compromiso con las personas
 						</h4>
 						<p>
-							con las personas: enfocarnos en el desarrollo personal y profesional de las personas es sustancial para mejorar la gestión pública. Son las personas las que actúan en todos los niveles del Estado y del gobierno y por tanto, afianzar sus conocimientos técnicos, profesionales y éticos son la clave para el desarrollo social.
+							Enfocarnos en el desarrollo personal y profesional de las personas es sustancial para mejorar la gestión pública. Son las personas las que actúan en todos los niveles del Estado y del gobierno y por tanto, afianzar sus conocimientos técnicos, profesionales y éticos son la clave para el desarrollo social.
 						</p>
 						<hr class="pi-divider-gap-10">
 					</div>
@@ -294,37 +368,49 @@
 		<div class="pi-row">
 
 			<!-- Value item -->
-			<div class="pi-col-sm-4">
+			<div class="pi-col-sm-12">
 				<div class="pi-icon-box">
-					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #ab71bd;">
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
 						<i class="icon-target"></i>
 					</div>
-					<div class="pi-icon-box-content-2">
+					<div class="pi-icon-box-content-2 justi">
 						<h4>
-							ETICA
+							Ética
 						</h4>
 						<p>
-							las elecciones personales de todos los involucrados en la actividad pública son determinantes para el desarrollo. Por eso, es necesario fomentar y promover que las elecciones cotidianas y profesionales tengan en cuenta el resultado y las consecuencias a largo plazo y ante la duda, elegir hacer lo correcto.
+							Las elecciones personales de todos los involucrados en la actividad pública son determinantes para el desarrollo. Por eso, es necesario fomentar y promover que las elecciones cotidianas y profesionales tengan en cuenta el resultado y las consecuencias a largo plazo y ante la duda, elegir hacer lo correcto.
 						</p>
-						<hr class="pi-divider-gap-10">
+						
 					</div>
 				</div>
 			</div>
 			<!-- End value item -->
 
+		</div>
+
+<hr class="pi-divider-gap-60">
+
+		<h2 class="h4 pi-weight-700 pi-uppercase pi-letter-spacing pi-has-bg pi-margin-bottom-30">
+			líneas de Acción
+		</h2>
+		<p><strong>OLAGED</strong> considera fundamental trabajar con valores éticos que guíen su actividad cotidiana, por eso, nos comprometemos a actuar con:</p> <br>
+<!-- Values -->
+		<div class="pi-row">
+
+
 			<!-- Value item -->
-			<div class="pi-col-sm-4">
+			<div class="pi-col-sm-6">
 				<div class="pi-icon-box">
-					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #54aaee;">
-						<i class="icon-briefcase"></i>
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
+						<i class="icon-bitcoin"></i>
 					</div>
-					<div class="pi-icon-box-content-2">
+					<div class="pi-icon-box-content-2 justi">
 						<h4>
 							LINEA DE ACCION 1
 						</h4>
 						<p>
 							Convocamos a profesionales competentes, con amplia experiencia y que estén comprometidos con el desarrollo de las políticas públicas. 
-						</p>
+						</p><br>
 						<hr class="pi-divider-gap-10">
 					</div>
 				</div>
@@ -332,13 +418,13 @@
 			<!-- End value item -->
 
 			<!-- Value item -->
-			<div class="pi-col-sm-4">
+			<div class="pi-col-sm-6">
 
 				<div class="pi-icon-box">
-					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #9dd05b;">
-						<i class="icon-leaf"></i>
+					<div class="pi-icon-box-icon pi-icon-box-icon-square pi-icon-box-icon-base" style="background: #12BCFF;">
+						<i class="icon-user"></i>
 					</div>
-					<div class="pi-icon-box-content-2">
+					<div class="pi-icon-box-content-2 justi">
 						<h4>
 							LINEA DE ACCION 2
 						</h4>
@@ -350,37 +436,13 @@
 				</div>
 			</div>
 			<!-- End value item -->
-
-		</div>
-		<!-- End values -->
-<h2 class="h4 pi-weight-700 pi-uppercase pi-letter-spacing pi-has-bg pi-margin-bottom-30">
-		</h2>
-
-
 	</div>
+
 </div>
 
-<!-- - - - - - - - - - END SECTION - - - - - - - - - -->
-
-<!-- - - - - - - - - - SECTION - - - - - - - - - -->
-<div class="pi-section-w pi-shadow-inside-top pi-section-base">
-	<div class="pi-texture" style="background: url(img/hexagon.png) repeat;"></div>
-	<div class="pi-section pi-padding-top-60 pi-padding-bottom-40 pi-text-center">
-
-		<p class="lead-30">
-			<span class="pi-text-white">¿Tienes algún proyecto por realizar? <span class="pi-weight-400">Te ayudamos.</span></span>
-
-			<a href="contacto.html" class="btn pi-btn-base-2 pi-btn-big" style="margin-left: 25px;">
-				<i class="icon-mail pi-icon-left"></i> Contáctanos
-			</a>
-
-		</p>
-
-	</div>
-</div>
 <!-- - - - - - - - - - END SECTION - - - - - - - - - --></div>
 
-<?php include("include/footer.php"); ?>
+<?php include("include/footer2.php"); ?>
 
 </body>
 </html>
