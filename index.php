f5e14126538640f47b8a5f1fb03378172df1dbe3
luizink <luizupdate@gmail.com>
2017-11-15 17:28:14 -0500

    Revert "topbar"
    This reverts commit b8758ff4dd476daf26841cf85d49a0b0a2939448.

--- index.php
+++ index.php
@@ -1,6 +1,95 @@
-<?php 
-include("include/topbar.php");
- ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
+	<title>OLAGED - Perú</title>
+
+	<link rel="shortcut icon" href="images/favicon.ico">
+
+	<link rel="stylesheet" type="text/css" href="css/style.css"/>
+	<link rel="stylesheet" type="text/css" href="css/loader.css"/>
+	<link rel="stylesheet" type="text/css" href="3dParty/rs-plugin/css/pi.settings.css"/>
+	<link rel="stylesheet" type="text/css" href="css/slider.css"/>
+	<link rel="stylesheet" type="text/css" href="css/counters.css"/>
+	<link rel="stylesheet" type="text/css" href="css/galleries.css"/>
+	<link rel="stylesheet" type="text/css" href="css/images.css"/>
+	<link rel="stylesheet" type="text/css" href="css/portfolio.css"/>
+	<link rel="stylesheet" type="text/css" href="css/alert-boxes.css"/>
+	<link rel="stylesheet" type="text/css" href="css/animations.css"/>
+	<link rel="stylesheet" type="text/css" href="3dParty/colorbox/colorbox.css"/>
+	<link href="css/bootstrap.css" rel="stylesheet">
+
+	<!--Fonts-->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,100,300,600,700,800&amp;subset=latin,cyrillic'
+		  rel='stylesheet' type='text/css'>
+
+	<!--Fonts with Icons-->
+	<link rel="stylesheet" href="3dParty/fontello/css/fontello.css"/>
+
+	<!--[if lte IE 8]>
+	<script src="scripts/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+<div id="pi-all">
+
+<!-- Header -->
+<div class="pi-main-header-w">
+
+<!-- Header row -->
+<div class="pi-section-w pi-section-header-w pi-section-dark1 pi-section-header-sm-w">
+	<div class="pi-section pi-section-header pi-section-header-sm pi-clearfix">
+
+		<!-- Phone -->
+		<div class="pi-header-block pi-header-block-txt">
+			<i class="pi-header-block-icon icon-phone pi-icon-base pi-icon-square"></i>Informes: <strong>+51 999337660</strong>
+		</div>
+		<!-- End phone -->
+
+		<!-- Email -->
+		<div class="pi-header-block pi-header-block-txt pi-hidden-xs"><i
+				class="pi-header-block-icon icon-mail pi-icon-base pi-icon-square"></i>Email: <a href="#">info@olaged.com</a>
+		</div>
+		<!-- End email -->
+
+		<!-- Social icons -->
+		<div class="pi-header-block pi-pull-right pi-hidden-2xs">
+			<ul class="pi-social-icons pi-stacked pi-jump pi-full-height pi-bordered  pi-colored-bg clearFix">
+				<li><a href="https://www.linkedin.com/uas/login" target="_blank" class="pi-social-icon-linkedin"><i class="icon-linkedin"></i></a></li>
+				<li><a href="https://www.facebook.com/" target="_blank" class="pi-social-icon-facebook"><i class="icon-facebook"></i></a></li>
+				<li><a href="https://www.youtube.com/" target="_blank" class="pi-social-icon-youtube"><i class="icon-youtube"></i></a></li>
+				<li><a href="https://twitter.com/login?lang=es" target="_blank" class="pi-social-icon-twitter"><i class="icon-twitter"></i></a></li>
+				
+				
+			</ul>
+		</div>
+		<!-- End social icons -->
+
+		<!-- Text -->
+		<div class="pi-header-block pi-header-block-txt pi-pull-right pi-hidden-xs">Síguenos en:</div>
+		<!-- End text -->
+
+	</div>
+</div>
+<!-- End header row -->
+
+<div class="pi-header-row-sticky-w">
+<!-- Header row -->
+<div class="pi-section-w pi-section-header-w pi-section-white pi-section-header-lg-w pi-header-row-sticky pi-shadow-bottom">
+<div class="pi-section pi-section-header pi-section-header-lg pi-clearfix">
+
+<!-- Logo -->
+<div id="mejor" class="pi-header-block pi-header-block-logo">
+	<a href="index.php"><img src="images/logo.svg" alt=""></a>
+</div>
+<!-- End logo -->
+
+<!-- Text -->
+<!-- <div class="pi-header-block pi-header-block-txt pi-hidden-2xs">Observatorio Latinoamericano de Gestión y Desarrollo</div> -->
+<!-- End text -->
 
 <!-- Menu -->
 <?php include("include/navegacion.php"); ?>
